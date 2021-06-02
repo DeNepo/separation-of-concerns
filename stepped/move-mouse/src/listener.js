b1646ee05c85f1fb3636b13d912eee4238b6fcbc
Sheyla <shel.busta@gmail.com>
2021-06-02 16:11:18 +0200

    review-move-mouse-myself

---
+++ stepped/move-mouse/src/listener.js
@@ -0,0 +1,12 @@
+import { shadow } from "./handler.js";
+//import { hero } from "./handler.js";
+document;
+// .getElementById("bighero")
+// .addEventListener("mouseover", (event) => console.log(event));
+
+const hero = document
+  .querySelector(".hero")
+  .addEventListener("mouseover", shadow);
+
+// const text = hero.querySelector("h1");
+// const walk = 500; // 500px
